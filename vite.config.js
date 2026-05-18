749a9cdea70c7903d372ea4c5607c588f2cc1d0f
tamer.naffaa <tamer.n.co@gmail.com>
2026-05-18 22:21:40 +0300

    Fix vite.config.js

---
+++ vite.config.js
@@ -0,0 +1,25 @@
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+import tailwindcss from '@tailwindcss/vite'
+import { nodePolyfills } from 'vite-plugin-node-polyfills'
+
+// https://vite.dev/config/
+export default defineConfig({
+  plugins: [
+    react(),
+    tailwindcss(),
+    nodePolyfills({
+      include: ['events', 'process', 'buffer', 'util', 'stream'],
+      globals: {
+        Buffer: true,
+        global: true,
+        process: true,
+      },
+    }),
+  ],
+  define: {
+    // By default, Vite doesn't include process.env
+    'process.env': {},
+    global: 'globalThis',
+  }
+})
